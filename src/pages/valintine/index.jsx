dd0b446251ffe3269188563bff36f28c27f91890
Florian Qengaj <florian.qengaj@gmail.com>
2024-02-11 11:36:23 +0100

    first part done.

--- src/pages/valintine/index.jsx
+++ src/pages/valintine/index.jsx
@@ -1,5 +1,6 @@
 import "./index.css";
-import data from '../../res/content/texts.json'
+import dataPos from '../../res/content/texts.json'
+import dataNeg from '../../res/content/failtexts.json'
 import { useState } from "react";
 // import images from '../../res/img/'
 
@@ -10,67 +11,80 @@ function importAll(r) {
     return images;
 }
 
-const images = importAll(require.context('../../res/img', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../../res/img', false, /\.(png|jpe?g|svg|gif)$/));
 
 // console.log(images)
 
 export const Valentine = ()=>{
     const [step, setStep] = useState(0)
     const [stage, setStage] = useState(0)
+    const [isPosArc, setPosArc] = useState(true)
+    const [negObj, setNegObj] = useState(0)
     // style={{backgroundImage: `url(${images[data[stage][step].img]})`}}
+    let currObj = (isPosArc) ? dataPos[stage][step] : dataNeg[negObj] 
     return <>
     <div data-theme="valentinstag" className="valentine-body" >
-        <div className="card w-96 h-full mt-10 mb-16 bg-base-100 shadow-2xl image-full">
-            <figure><img src={(data[stage][step].img)? images[data[stage][step].img] :"https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Hey Pupsie! ❤️</h2>
-                <p key={(stage, step)}>{data[stage][step].txt}</p>
-                <div className="card-actions justify-between items-center">
-                    <button className="btn btn-primary btn-lg"
-                    onClick={()=>{
+    <div className="card w-80 h-auto mt-10 mb-16 bg-base-100 shadow-2xl ">
+        <figure><img style={{aspectRatio: 3/4, objectFit:"cover" }} src={(currObj.img)? images[currObj.img] :""} /></figure>
+        <div className="card-body">
+            <h2 className="card-title">{currObj.ttxt ? currObj.ttxt : "no title yet"}</h2>
+            <p>{currObj.txt}</p>
 
-                        if (step < data[stage].length-1){
+            <div className="card-actions justify-between items-center">
+                {currObj.posbtn ? <button className="btn btn-primary btn-lg"
+                onClick={()=>{
+                    if (isPosArc){
+                        if (step < dataPos[stage].length-1){
                             setStep(step+1)
                         }else{
                             setStep(0)
-                            if (stage < data.length-1){
+                            if (stage < dataPos.length-1){
                                 setStage(stage+1)
                             }else{
                                 setStage(0)
                             }
                         }
-                        
-                    }}>{data[stage][step].posbtn}</button>
-                    {data[stage][step].negbtn !== "" ? <button id="btn-no" className="btn btn-xs">{data[stage][step].negbtn}</button>:""}
-                </div>
+                    }else{
+                        // war im negArc -> positiv
+                        setPosArc(true)
+                        setStage(currObj.nextPos.stage)
+                        setStep(currObj.nextPos.step)
+                    }
+                    
+                }}>{currObj.posbtn}</button>
+                : ""}
+                {
+                currObj.negbtn !== "" ? 
+                    <button onClick={()=>{
+                        setNegObj(currObj.nextNeg)
+                        setPosArc(false)
+                    }} className="btn btn-xs">{currObj.negbtn}</button>
+                :
+                    ""
+                }
             </div>
         </div>
+    </div>
     </div>
     <div className="stepsContainer">
         <ul className="steps">
             {
-                data.map((value, currIndex)=>{
+                dataPos.map((value, currIndex)=>{
                     if (currIndex < stage){
-                        return <li key={currIndex} data-content="❤️" className="step step-secondary">{currIndex}</li>
+                        return <li onClick={()=>{
+                            setStage(currIndex)
+                            setStep(0)
+                        }} key={currIndex} data-content="❤️" className="step step-success"></li>
+                    }else if (currIndex === stage){
+                        return <li onClick={()=>{
+                            setStage(currIndex)
+                            setStep(0)
+                        }} key={currIndex} data-content="" className="step step-info"></li>
                     }else{
-                        return <li key={currIndex} data-content="" className="step step-neutral">{currIndex}</li>
+                        return <li key={currIndex} data-content="" className="step step-neutral"></li>
                     }
                 })
             }
-            {/* {txts.map((_, i)=>{
-                if (i < txt){
-                    return <li data-content="✓" className="step step-secondary">{i}</li>
-                }else{
-                    return <li data-content="" className="step step-neutral">{i}</li>
-                }
-            })} */}
-            {/* <li data-content="?" className="step step-secondary">Step 1</li>
-            <li data-content="!" className="step step-secondary">Step 2</li>
-            <li data-content="✓" className="step step-secondary">Step 3</li>
-            <li data-content="✕" className="step step-secondary">Step 4</li>
-            <li data-content="★" className="step step-secondary">Step 5</li>
-            <li data-content="" className="step step-neutral">Step 6</li>
-            <li data-content="●" className="step step-neutral">Step 7</li> */}
         </ul>
     </div>
     </>
